120b6e797f3e9d5bebd89ced10dc59b2fc399812
GalahadWang <2685707164@qq.com>
2024-03-17 13:19:34 +1100

    ChangeToWorK

--- src/main/java/com/comp5703/yearnmove/controller/HelloWorld.java
+++ src/main/java/com/comp5703/yearnmove/controller/HelloWorld.java
@@ -2,12 +2,14 @@ package com.comp5703.yearnmove.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@RequestMapping("/hello")
+@RestController
+//@RequestMapping("/hello")
 public class HelloWorld {
-    @RequestMapping("/helloworld")
-    public String helloworld(){
+    @RequestMapping("/hello")
+    public String hello(){
+        System.out.println("Hello World");
         return "Hello World";
     }
 }
